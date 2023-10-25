3bbdd1e393cbe7647e2a82c84b9a26a0f4d6dc9a
werrons <bakytsanzirov13@gmail.com>
2023-10-25 14:08:41 +0600

    polnyi

--- script.js
+++ script.js
@@ -6,6 +6,25 @@
 // symbol =
 // bigint =
 
-let a = 'privet'
-let b = ' bratik'
-console.log(a + b);
+// let a = 4
+// let b = 55
+// console.log(a + b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// let a = 'hello'
+// let b = ' aidin'
+// console.log( a + b)
+
+// let a = 'privet'
+// let b = 3
+// console.log(a - b)
+
+// let a = 5
+// let b = '5'
+// console.log(a + + b)
+
+let c = 2
+let b = undefined
+console.log(c + b)
